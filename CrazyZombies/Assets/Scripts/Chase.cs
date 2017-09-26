3149241e043cc4d24e086e892ace4e4b70c534ab
Yu Sun <yus471@mail.usask.ca>
2017-09-25 19:50:43 -0600

    Update map init script that able to generate map with specified size

---
+++ CrazyZombies/Assets/Scripts/Chase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour {
+
+	public GameObject target;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 v = new Vector2 ();
+		v.x = target.transform.position.x - transform.position.x;
+		v.y = target.transform.position.y - transform.position.y;
+		if (v.magnitude != 0) {
+			Vector2 v2 = new Vector2 ();
+			v2.x = v.x * speed / v.magnitude * Time.deltaTime;
+			v2.y = v.y * speed / v.magnitude * Time.deltaTime;
+			GetComponent<Rigidbody2D> ().velocity = v2;
+		}
+	}
+}
--- CrazyZombies/Assets/Scripts/PlayerController.cs
+++ CrazyZombies/Assets/Scripts/PlayerController.cs
@@ -4,42 +4,34 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float playerMovingSpeed;
-	public float mouseRotationSpeed;
-	public GameObject mousePointer;
-	Rigidbody myRigidBody;
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
-		myRigidBody = GetComponent<Rigidbody> ();
+		animator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetAxisRaw ("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal")< 0.5f) {
-		
-			transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * playerMovingSpeed * Time.deltaTime, 0f, 0f));
-		
+		Vector2 v = new Vector2 ();
+		if (Input.GetButton ("Horizontal") && Input.GetAxisRaw("Horizontal") < 0) {
+			v.x = -200 * Time.deltaTime;
+		} else if (Input.GetButton ("Horizontal") && Input.GetAxisRaw("Horizontal") > 0) {
+			v.x = 200 * Time.deltaTime;
+		} else {
+			v.x = 0;
 		}
 
-		if (Input.GetAxisRaw ("Vertical") > 0.5f || Input.GetAxisRaw("Vertical")< 0.5f) {
-
-			transform.Translate ( new Vector3 (0f, Input.GetAxisRaw ("Vertical") * playerMovingSpeed * Time.deltaTime, 0f));
-
+		if (Input.GetButton ("Vertical") && Input.GetAxisRaw ("Vertical") < 0) {
+			v.y = -200 * Time.deltaTime;
+		} else if (Input.GetButton ("Vertical") && Input.GetAxisRaw ("Vertical") > 0) {
+			v.y = 200 * Time.deltaTime;
+		} else {
+			v.y = 0;
 		}
+		GetComponent<Rigidbody2D> ().velocity = v;
 
-		//float inputHorizontal = Input.GetAxis ("Horizontal");
-		//float inputVertical = Input.GetAxis ("Vertical");
-		//Vector3 newVelocity=new Vector3(inputVertical*playerMovingSpeed, 0.0f, inputHorizontal*-playerMovingSpeed);
-		//myRigidBody.velocity = newVelocity;
-
-		//Vector2 direction = Camera.main.ScreenToWorldPoint (Input.mousePosition) - transform.position;
-		//float angle = Mathf.Atan2 (direction.y, direction.x) * Mathf.Rad2Deg;
-		//Quaternion rotation = Quaternion.AngleAxis (angle, Vector3.forward);
-		//transform.rotation = Quaternion.Slerp (transform.rotation, rotation, mouseRotationSpeed * Time.deltaTime);
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		transform.rotation = Quaternion.LookRotation (Vector3.forward, mousePos - transform.position);
 	}
-
-
-
-
 }
