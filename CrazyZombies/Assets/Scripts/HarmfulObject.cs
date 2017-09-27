ff9a414f2a1ee6c0d2584f5e3008709b4bd1fe96
Yu Sun <yus471@mail.usask.ca>
2017-09-26 20:15:35 -0600

    Add hp script and damage script

---
+++ CrazyZombies/Assets/Scripts/HarmfulObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarmfulObject : MonoBehaviour {
+	public int damage; // How harmful this object is
+	public string harmTarget; // What this object harm for
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject player = GameObject.FindGameObjectWithTag (harmTarget);
+		if (GetComponent<Rigidbody2D> ().IsTouching (player.GetComponent<Collider2D>()) && player.GetComponent<MortalObject>() != null) {
+			player.SendMessage ("takeDamage", damage);
+		}
+	}
+}
