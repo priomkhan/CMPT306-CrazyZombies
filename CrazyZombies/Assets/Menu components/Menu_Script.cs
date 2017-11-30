e8aab01185e66c6f76325d6440ec2d289433e282
qag014 <qammergull@hotmail.com>
2017-11-30 12:04:12 -0600

    Menu
    Added Menu to Branch

---
+++ CrazyZombies/Assets/Menu components/Menu_Script.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_Script : MonoBehaviour {
+
+	public AudioSource audio;
+	void Start () {
+		audio = GetComponent<AudioSource>();
+		audio.Play ();
+
+	}
+	// Use this for initialization
+	public void PlayGame () {
+		SceneManager.LoadScene(1);
+
+	}
+
+	// Update is called once per frame
+	public void Quit () {
+		Application.Quit();
+	}
+}
